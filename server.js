92da0a9be5570ae43c5afae09bb9b2c91d137e2e
Kevin Souza <kevinxsouza@gmail.com>
2022-11-22 16:03:49 -0500

    heroku test

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.get('/', (req, res) => {
 // mongodb://localhost:27017/movieTracker
 // heroku"
 // mongodb+srv://kevanks:Berserk2018@cluster0.fqh55jt.mongodb.net/?retryWrites=true&w=majority
-mongoose.connect('mongodb://localhost:27017/movieTracker', () => {
+mongoose.connect('mongodb+srv://kevanks:Berserk2018@cluster0.fqh55jt.mongodb.net/?retryWrites=true&w=majority', () => {
   console.log('Connected to Mongo');
 });
 
