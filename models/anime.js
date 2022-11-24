052207081593182933db9b4510d5373dd0235b91
Kevin Souza <kevinxsouza@gmail.com>
2022-11-24 15:21:54 -0500

    7 routes for anime added

---
+++ models/anime.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const animeSchema = new mongoose.Schema({
+  title: String,
+  year: Number,
+  numSeasons: Number,
+  mangaka: String,
+  genre: String,
+  studio: String,
+  watched: Boolean,
+  rating: {type: Number, min: 1, max:5},
+  comments: String
+})
+
+const Anime = mongoose.model('Anime', animeSchema)
+
+module.exports = Anime
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const Movie = require('./models/movie.js');
 const Tv = require('./models/tv.js');
+const Anime = require('./models/anime.js');
 const app = express();
 
 
@@ -19,6 +20,13 @@ if(process.env.PORT) {
 }
 
 // DELETE ROUTES ////////////////////////////////////////////////////////////////
+// deleting anime route
+app.delete('/anime/:id', (req, res) => {
+  Anime.findByIdAndRemove(req.params.id, (err, deletedAnime) => {
+    res.redirect('/anime')
+  })
+});
+
 // deleting tv route
 app.delete('/tv/:id', (req, res) => {
   Tv.findByIdAndRemove(req.params.id, (err, deletedTv) => {
@@ -34,6 +42,18 @@ app.delete('/:id', (req, res) => {
 });
 
 // EDITING ROUTES ///////////////////////////////////////////////////////////////
+// adding edited anime route
+app.put('/anime/:id', (req, res) => {
+  if (req.body.watched === 'on') {
+    req.body.watched = true
+  } else {
+    req.body.watched = false
+  }
+  Anime.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedAnime) => {
+    res.redirect('/anime')
+  })
+});
+
 // adding edited tv route
 app.put('/tv/:id', (req, res) => {
   if (req.body.watched === 'on') {
@@ -59,6 +79,14 @@ app.put('/:id', (req, res) => {
   })
 });
 
+// anime edit page route
+app.get('/anime/:id/edit', (req, res) => {
+  Anime.findById(req.params.id, (err, foundAnime) => {
+    res.render('animeEdit.ejs',
+    {foundAnime: foundAnime})
+  })
+});
+
 // tv edit page route
 app.get('/tv/:id/edit', (req, res) => {
   Tv.findById(req.params.id, (err, foundTv) => {
@@ -76,6 +104,18 @@ app.get('/:id/edit', (req, res) => {
 });
 
 // NEW PAGE ROUTES //////////////////////////////////////////////////////////////
+// adds new anime to the anime index
+app.post('/anime', (req, res) => {
+  if (req.body.watched === 'on') {
+    req.body.watched = true
+  } else {
+    req.body.watched = false
+  }
+  Anime.create(req.body, (err, addedAnime) => {
+    res.redirect('/anime')
+  })
+});
+
 // adds new tv to tv tv index
 app.post('/tv', (req, res) => {
   if (req.body.watched === 'on') {
@@ -100,6 +140,11 @@ app.post('/', (req, res) => {
   })
 });
 
+// anime new page route
+app.get('/anime/new', (req, res) => {
+  res.render('animeNew.ejs')
+});
+
 // Tv new page route
 app.get('/tv/new', (req, res) => {
   res.render('tvNew.ejs')
@@ -112,6 +157,14 @@ app.get('/new', (req, res) => {
 });
 
 // INDEX PAGE ROUTES ////////////////////////////////////////////////////////////
+// anime index page route
+app.get('/anime', (req, res) => {
+  Anime.find({}, (err, allAnime) => {
+    res.render('animeIndex.ejs',
+    {allAnime: allAnime})
+  })
+});
+
 // tv index page route
 app.get('/tv', (req, res) => {
   Tv.find({}, (err, allTv) => {
@@ -129,6 +182,14 @@ app.get('/', (req, res) => {
 });
 
 // SHOW PAGE ROUTES /////////////////////////////////////////////////////////////
+// anime show page route
+app.get('/anime/:id', (req, res) => {
+  Anime.findById(req.params.id, (err, foundAnime) => {
+    res.render('animeShow.ejs',
+    {foundAnime: foundAnime})
+  })
+});
+
 // tv show page route
 app.get('/tv/:id', (req, res) => {
   Tv.findById(req.params.id, (err, foundTv) => {
